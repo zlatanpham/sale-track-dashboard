b9ab35e9d9416446172f10bd62a9f7f70a5b15aa
zlatan <zlatan.pham@gmail.com>
2019-04-01 16:09:24 +0700

    update toggle style

--- src/components/RightPane/ThemeToggle.js
+++ src/components/RightPane/ThemeToggle.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Toggle } from '../Toggle';
+import tw from 'tailwind.macro';
 
 const Container = styled.div`
   padding: 40px;
+  ${tw`flex items-center justify-end`}
 `;
 
 export const ThemeToggle = ({ themeToggled, onThemeToggle }) => {
   return (
     <Container>
+      <span
+        className={
+          'mr-2 mb-1 font-semibold text-sm' +
+          (themeToggled ? '' : ' opacity-50 line-through')
+        }
+      >
+        Dark mode
+      </span>
       <Toggle toggled={themeToggled} onToggle={onThemeToggle} />
     </Container>
   );
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { FaToggleOff, FaToggleOn } from 'react-icons/fa';
 
 export const Toggle = ({ toggled, onToggle }) => (
-  <button onClick={onToggle}>{toggled ? 'toggled' : 'untoggled'}</button>
+  <button
+    onClick={onToggle}
+    css={`
+      color: ${props => props.theme.main.foreground};
+      font-size: 22px;
+      line-height: 1em;
+    `}
+  >
+    {toggled ? <FaToggleOn /> : <FaToggleOff />}
+  </button>
 );
