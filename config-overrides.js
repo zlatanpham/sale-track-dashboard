e0a95be397fb9ef68d95f57dae7cce3786a2e1fb
zlatan <zlatan.pham@gmail.com>
2019-04-01 11:20:36 +0700

    config: fix tailwind not work in className

--- config-overrides.js
+++ config-overrides.js
@@ -4,11 +4,13 @@ const {
   addWebpackResolve,
   addBabelPlugin,
   override,
+  addPostcssPlugins,
 } = require('customize-cra');
 const path = require('path');
 
 module.exports = override(
   addWebpackResolve({ alias: { '@': path.resolve(__dirname, 'src') } }),
   addBabelPlugin('babel-plugin-styled-components'),
+  addPostcssPlugins([require('tailwindcss')('./src/tailwind.js')]),
   config => rewireReactHotLoader(config),
 );
