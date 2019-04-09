726c87cacbd8ce6e285b03d1dcd07be4a253c340
zlatan <zlatan.pham@gmail.com>
2019-04-09 16:01:18 +0700

    config: remove unused css with purgeCSS

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+let PurgecssPlugin = require('purgecss-webpack-plugin');
+let glob = require('glob-all');
 
 const {
   addWebpackResolve,
@@ -8,6 +10,12 @@ const {
   addPostcssPlugins,
 } = require('customize-cra');
 const path = require('path');
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 module.exports = override(
   addWebpackResolve({ alias: { '@': path.resolve(__dirname, 'src') } }),
@@ -21,6 +29,15 @@ module.exports = override(
           openAnalyzer: false,
           generateStatsFile: true,
           statsFilename: 'stats.json',
+        }),
+        new PurgecssPlugin({
+          paths: glob.sync([path.join(__dirname, 'src/**/*.js')]),
+          extractors: [
+            {
+              extractor: TailwindExtractor,
+              extensions: ['html', 'js'],
+            },
+          ],
         }),
       ]);
     }
