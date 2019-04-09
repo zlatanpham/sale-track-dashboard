e09791eb292a7228ddce3d6295724460e7220be7
zlatan <zlatan.pham@gmail.com>
2019-04-09 15:48:20 +0700

    add analyze-bundler

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const {
   addWebpackResolve,
@@ -12,5 +13,18 @@ module.exports = override(
   addWebpackResolve({ alias: { '@': path.resolve(__dirname, 'src') } }),
   addBabelPlugin('babel-plugin-styled-components'),
   addPostcssPlugins([require('tailwindcss')('./src/tailwind.js')]),
+  (config, env) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins = (config.plugins || []).concat([
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          openAnalyzer: false,
+          generateStatsFile: true,
+          statsFilename: 'stats.json',
+        }),
+      ]);
+    }
+    return config;
+  },
   config => rewireReactHotLoader(config),
 );
